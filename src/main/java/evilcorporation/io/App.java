bc1b9c7b5856104e640718eb11fe7a869afb9892
izabela1337 <54004218+izabela1337@users.noreply.github.com>
2023-06-06 02:10:08 +0200

    a

--- src/main/java/evilcorporation/io/App.java
+++ src/main/java/evilcorporation/io/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "I'm evil, but in Java" );
+        System.out.println( "I'm evil, but in Java2" );
     }
 }
